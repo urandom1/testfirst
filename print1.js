355a328cfab2c32866770675bb3e98fff92e089f
urandom1 <34662722+urandom1@users.noreply.github.com>
2017-12-30 20:08:40 -0800

    Add files via upload

---
+++ print1.js
@@ -0,0 +1,19 @@
+﻿function G() {
+   this.fail = 'bad';
+}
+
+function F() { 
+   this.test = 'ok';
+}
+
+F.prototype = G;
+F.constructor = G;
+
+var f = new F();  // Prints F
+
+console.log(f);
+
+f.prototype = G;  // Redefining f type info
+f.constructor = G;
+
+console.log(f);  // Prints G
